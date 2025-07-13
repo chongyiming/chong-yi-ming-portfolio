123d5f82ebbcaa2acc45a3d4cb305e81e3eda3c2
chong yi ming <chongyiming1205@gmail.com>
2025-07-13 17:21:47 +0800

    test

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello, Next.js!
-    </h1>
+    <>
+      <Navbar />
+    </>
   );
 }
